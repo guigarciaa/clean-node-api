7d21687a5200d7567c4a113354374a9b245a063d
Guilherme Garcia <iamguigarcia@gmail.com>
2020-11-01 22:59:41 -0300

    test: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.controller.ts
+++ src/presentation/controllers/signup/signup.controller.ts
@@ -1,7 +1,6 @@
-import { HttpResponse, HttpRequest, Controller, EmailValidator } from './signup-protocols'
+import { HttpResponse, HttpRequest, Controller, EmailValidator, AddAccount } from './signup-protocols'
 import { MissingParamError, InvalidParamError } from '../../error'
 import { badRequest, serverError } from '../../helpers/http-helper'
-import { AddAccount } from '../../../domain/usecases/add-account'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
