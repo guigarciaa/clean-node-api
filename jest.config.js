4360d072e53c5ba7bd102dda5cc9fee28031f036
Guilherme Garcia <iamguigarcia@gmail.com>
2020-11-04 18:07:19 -0300

    refactor: refactor export jest and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
